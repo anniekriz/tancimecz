b938b4626b972daba51bbc2c80d5269894b6bdeb
ANNIE\annau <anna.ukrizu@gmail.com>
2024-07-15 11:47:13 +0200

    lektor může upravit svůj profil

--- web/dance_web/danceapp/admin.py
+++ web/dance_web/danceapp/admin.py
@@ -2,8 +2,8 @@ from django.contrib import admin
 from .models import Event, Lector, Location
 
 #admin.site.register(Event)
-admin.site.register(Lector)
-#admin.site.register(Location)
+#admin.site.register(Lector)
+admin.site.register(Location)
 
 class EventAdmin(admin.ModelAdmin):
     list_display = ('title', 'start', 'end', 'lector')
@@ -28,6 +28,29 @@ class EventAdmin(admin.ModelAdmin):
         super().save_model(request, obj, form, change)
 
 admin.site.register(Event, EventAdmin)
+
+class LectorAdmin(admin.ModelAdmin):
+    list_display = ('firstName', 'lastName')
+
+    def get_queryset(self, request):
+        qs = super().get_queryset(request)
+        if request.user.is_superuser:
+            return qs
+        try:
+            lector = Lector.objects.get(user=request.user)
+            return qs.filter(id=lector.id)
+        except Lector.DoesNotExist:
+            return qs.none()
+
+    def save_model(self, request, obj, form, change):
+        if not obj.pk:  # Only set the lector during the first save.
+            try:
+                obj.lector = Lector.objects.get(user=request.user)
+            except Lector.DoesNotExist:
+                pass  # Handle the case where the user does not have an associated Lector.
+        super().save_model(request, obj, form, change)
+
+admin.site.register(Lector, LectorAdmin)
 
 
 
