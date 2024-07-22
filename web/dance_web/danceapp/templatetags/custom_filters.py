947b0cbe1cf7c506dd90ca0bf37879d6a1ae4c29
ANNIE\annau <anna.ukrizu@gmail.com>
2024-07-22 15:02:36 +0200

    uprava datumu, proklikavaci lektori apod.

--- web/dance_web/danceapp/templatetags/custom_filters.py
+++ web/dance_web/danceapp/templatetags/custom_filters.py
@@ -16,14 +16,20 @@ def date_format(event):
     # zajistí, že místo 07:00 bude 7:00
     def format_time(t):
         return t.strftime('%H:%M').lstrip('0').replace(' 0', ' ')
+    
+    def format_date(d):
+        return f"{d.day}.{d.month}."
 
     if start.date() == end.date():
         # %H: Hodina ve 24hodinovém formátu (00 až 23), %M: Minuta (00 až 59)
-        date = f"{start.strftime('%d.%m. %Y')}"
+        date = f"{start.strftime('%d.%m.')}"
         day = f"{DAYS_SHORT[start.weekday()]}"
         time = f"{format_time(start)}-{format_time(end)}"
     else:
-        date = f"{start.strftime('%d.%m.')}–{end.strftime('%d.%m. %Y')}"
+        if start.month == end.month:
+            date = f"{start.day}–{end.day}.{start.month}."
+        else:
+            date = f"{start.day}.{start.month}.–{end.day}.{end.month}."
         day = f"{DAYS_SHORT[start.weekday()]}-{DAYS_SHORT[end.weekday()]}"
         time = None
 
