66d0265e974fd3fe88b8c14f73710226ccd3c998
ANNIE\annau <anna.ukrizu@gmail.com>
2024-10-25 16:49:03 +0200

    cistka

--- web/dance_web/__backup/danceapp/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Event, Lector, Location
-
-admin.site.register(Event)
-admin.site.register(Lector)
-admin.site.register(Location)
-
--- web/dance_web/__backup/danceapp/templatetags/custom_filters.py
+++
@@ -1,7 +0,0 @@
-from django import template
-
-register = template.Library()
-
-@register.filter
-def filter_by_type(events, event_type):
-    return [event for event in events if event.type == event_type]
