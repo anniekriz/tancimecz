5c4d6b315bbe6dee901726b80f3e61ab98e547fc
Lubos Kriz <ukrizu@gmail.com>
2024-11-14 16:52:58 +0100

    Delete backup

--- web/dance_web/__backup/app.py
+++
@@ -1,8 +0,0 @@
-import os
-
-# Pokud se settings nachází v /srv/app/moje_aplikace,
-# bude obsah pro DJANGO_SETTINGS_MODULE: moje_aplikace.settings
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "dance_web.settings")
-
-from django.core.wsgi import get_wsgi_application
-application = get_wsgi_application()
--- web/dance_web/__backup/danceapp/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-
-class Search(forms.Form):
-   query = forms.CharField(required=False, label='Search')
-
-class Search_lectors(forms.Form):
-   query = forms.CharField(required=False, label='Search_lectors')
