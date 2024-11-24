49936fb731494687dab4c8b887d6a6cdf75f32a6
ANNIE\annau <anna.ukrizu@gmail.com>
2024-11-24 18:02:00 +0100

    lector page tance views

--- web/dance_web/dance_web/views.py
+++ web/dance_web/dance_web/views.py
@@ -67,9 +67,9 @@ def lector_page(request, slug):
     lector_index = list(lectors).index(lector)
     prev_lector = lectors[lector_index - 1] if lector_index > 0 else None
     next_lector = lectors[lector_index + 1] if lector_index < len(lectors) - 1 else None
-    events = Event.objects.all().order_by('date')
-    workshops = Workshop.objects.all().order_by('start')
-    combined = list(set(chain(events, workshops))) 
+    events = Event.objects.filter(parent__lector=lector).order_by('date')
+    workshops = Workshop.objects.filter(lector=lector).order_by('start')
+    combined = list(set(chain(events, workshops)))  # Ensure combined list is unique
     combined.sort(key=lambda x: getattr(x, 'date', getattr(x, 'start', None)))
     context = {
         'workshops': workshops,
