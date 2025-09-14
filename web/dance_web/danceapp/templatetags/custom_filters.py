3a5927d8006cae5a832c2c18f22146ce2333fb88
ANNIE\annau <anna.ukrizu@gmail.com>
2025-09-14 22:47:13 +0200

    Archive

--- web/dance_web/danceapp/templatetags/custom_filters.py
+++ web/dance_web/danceapp/templatetags/custom_filters.py
@@ -100,6 +100,48 @@ def date_format(event):
         'endTime': endTime
     }
 
+@register.filter
+def date_format_year(event):
+    if hasattr(event, 'start') and hasattr(event, 'end'):
+        start = timezone.localtime(event.start) if isinstance(event.start, timezone.datetime) else event.start
+        end = timezone.localtime(event.end) if isinstance(event.end, timezone.datetime) else event.end
+    else:
+        start = event.date
+        end = event.date
+
+    def format_time(t):
+        return t.strftime('%H:%M').lstrip('0').replace(' 0', ' ')
+
+    def format_date(d):
+        return f"{d.day}.{d.month}.{d.year}"
+
+    if start == end:
+        date = format_date(start)
+        day = f"{DAYS_SHORT[start.weekday()]}"
+        if isinstance(event, Event):
+            startTime = format_time(event.parent.startTime)
+            endTime = format_time(event.parent.endTime) if event.parent.endTime else None
+        else:
+            startTime = None
+            endTime = None
+    else:
+        if start.month == end.month and start.year == end.year:
+            date = f"{start.day}.–{end.day}.{end.month}.{end.year}"
+        elif start.year == end.year:
+            date = f"{start.day}.{start.month}.–{end.day}.{end.month}.{end.year}"
+        else:
+            date = f"{start.day}.{start.month}.{start.year}–{end.day}.{end.month}.{end.year}"
+        day = f"{DAYS_SHORT[start.weekday()]}-{DAYS_SHORT[end.weekday()]}"
+        startTime = None
+        endTime = None
+
+    return {
+        'date': date,
+        'day': day,
+        'startTime': startTime,
+        'endTime': endTime
+    }
+
 
 @register.filter
 def is_instance(obj, class_name):
